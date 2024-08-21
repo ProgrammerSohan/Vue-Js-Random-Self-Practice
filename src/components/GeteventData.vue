f6bb088d7b148c1fcbde8c07ce4a2bab52483a3b
ProgrammerSohan <sohanofficial2022@gmail.com>
2024-08-21 23:41:28 +0600

    get event data

---
+++ src/components/GeteventData.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h3>Get Event Data</h3>
+        <button @click="getData">Click for Event Data</button>
+    </div>
+</template>
+<script>
+export default {
+    name: 'GeteventData',
+    methods:{
+        getData(event){
+            const {target} = event;
+            console.log(event);
+            target.style.backgroundColor = 'green';
+        }
+
+    }
+    /*,
+    props:{
+        msg: String,
+    }*/
+   
+}
+</script>
+<style>
+    
+</style>
